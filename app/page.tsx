95c5f5d5421b70cf7acff54e2e051b3128bfb3db
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-07-20 23:59:55 +0300

    Screens

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ const pacifiko = Pacifico({ subsets: ["cyrillic"], weight: "400" });
 export default function Home() {
   return (
     <>
-      <header className="bg-mycolor3 p-4">
+      <header className="bg-mycolor3 p-4 border-b-2 border-b-red-600  shadow-2xl">
         <div className="container mx-auto flex flex-row justify-between items-center ">
-          <div className="text-white text-lg font-semibold">
+          <div className="text-white text-lg md:text-xl  font-semibold">
             logo
           </div>
 
-          <div>
-            <input type="text" />
+          <div className="hidden md:block md:w-1/3 lg:w-1/2">
+            <input type="text" className="w-full" />
           </div>
 
 
